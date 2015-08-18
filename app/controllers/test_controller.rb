9eaf9315d39df54b35f128fdc7180d46d33d7cd9
Root <sonali@ignisitsolutions.com>
2015-08-18 16:27:51 +0530

    asd

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,13 +1,16 @@
 class TestController < ApplicationController
   def show
 
-   # mail = Mail.defaults do
-    #  retriever_method :pop3, :address    => "mail.ignisitsolutions.com",
-   #                    :port       => 110,
-    #                   :user_name  => 'ankit@ignisitsolutions.com',
-     #                  :password   => 'ankit12345',
-     #                  :enable_ssl => false
-   # end
+   mail = Mail.defaults do
+     retriever_method :pop3, :address    => "mail.ignisitsolutions.com",
+                      :port       => 110,
+                      :user_name  => 'ankit@ignisitsolutions.com',
+                      :password   => 'ankit12345',
+                      :enable_ssl => false
+   end
+byebug
+   @emails = Mail.find(:what => :first, :count => 10, :order => :asc)
 
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'setting/mailDetails'
+  get 'setting/mailDetails'=> "settings"
 
   post 'setting/create'
 
